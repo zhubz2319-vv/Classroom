c1ef5a4be233e3bb18874affefc8968d217405ad
DueDine <duedine@outlook.com>
2024-12-05 20:59:28 +0800

    End

--- IEMS5725_Classroom/app/src/main/java/com/example/iems5725_Classroom/MyFirebaseMessagingService.kt
+++ IEMS5725_Classroom/app/src/main/java/com/example/iems5725_Classroom/MyFirebaseMessagingService.kt
@@ -1,7 +1,9 @@
 package com.example.iems5725_Classroom
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.Constants.MessageNotificationKeys.TAG
@@ -30,12 +32,17 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     private fun sendNotification(messageBody: String?, messageTitle: String?) {
         val channelId = "default_channel_id"
+        val intent = Intent(this, LoginActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.ic_launcher_foreground) // 设置你的通知图标
             .setContentTitle(messageTitle)
             .setContentText(messageBody)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
         notificationManager.notify(0, notificationBuilder.build())
     }
